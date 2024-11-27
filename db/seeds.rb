7fe53964d89a7bf0b8f56e7d3fc2f10023f5d1eb
Nominoe Girard <nominoe.girard@gmail.com>
2024-11-27 17:04:50 -0300

    airbnb simplified done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,35 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'My beautiful house in france',
+  address: '10 rue des mouettes',
+  description: 'Description',
+  price_per_night: 100,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'My beautiful house in USA',
+  address: '10 driver road',
+  description: 'Just another useful description that you will read',
+  price_per_night: 32,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'My beautiful house in brazil',
+  address: '25 rua do povo',
+  description: 'Just another useless description that you will read',
+  price_per_night: 50,
+  number_of_guests: 5
+)
